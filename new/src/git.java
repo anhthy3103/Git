5e65da892dbf6cb49d3710a7d8ab88871b9179b5
Thy <anhthy3103@gmail.com>
2016-11-04 12:27:35 +0700

    git commit

---
+++ new/src/git.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class git extends JFrame {
+	public git(){
+		setSize(400,400);
+		setLayout(null);
+		setTitle("git");
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		git f= new git();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+}
